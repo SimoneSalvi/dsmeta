24cf0f8a29f5a6c54e78aa1bedbb8fa6d242ed87
SimoneSalvi <simonebonemer@yahoo.com.br>
2022-09-10 14:40:08 -0300

    Send notification

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -19,15 +19,16 @@ function SalesCard() {
     const dmin = minDate.toISOString().slice(0, 10);
     const dmax = maxDate.toISOString().slice(0, 10);
 
-    console.log(dmin);
+    //    console.log(dmin);
 
-    axios.get(`${BASE_URL}/sales?minDate=${dmin}&maxDate=${dmax}`)
-    .then((response) => {
-      setSales(response.data.content);
+    axios
+      .get(`${BASE_URL}/sales?minDate=${dmin}&maxDate=${dmax}`)
+      .then((response) => {
+        setSales(response.data.content);
 
-      //    axios.get(`${BASE_URL}/sales`).then((response) => {
-      //     setSales(response.data.content);
-    });
+        //    axios.get(`${BASE_URL}/sales`).then((response) => {
+        //     setSales(response.data.content);
+      });
   }, [minDate, maxDate]);
 
   return (
@@ -79,7 +80,7 @@ function SalesCard() {
                   <td>{sale.amount.toFixed(2)}</td>
                   <td>
                     <div className="dsmeta-red-btn-container">
-                      <NotificationButton />
+                      <NotificationButton saleId={sale.id} />
                     </div>
                   </td>
                 </tr>
